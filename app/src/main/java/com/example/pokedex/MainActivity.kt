d89c8d55fbf7dbb0e78bfa16a572fe8dd57e0cb9
s224292 <s224292@student.dtu.dk>
2023-10-04 00:38:14 +0200

    Optimization of the buttons

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.pokedex
-
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -20,11 +23,20 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+//import com.example.pokedex.R.layout.activity_search_bar
 import com.example.pokedex.ui.theme.PokedexTheme
 
-class MainActivity : ComponentActivity() {
+open class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        // val Nav = findViewById<Button>(R.id.button);
+        //Nav.setOnClickListener(View.OnClickListener {
+
+        //val intent = Intent(this.SearchBarActivity::class.java)
+        //  startActivity(intent)
+        //})
         setContent {
             PokedexTheme {
                 // A surface container using the 'background' color from the theme
@@ -32,7 +44,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("GUYS")
+                    Greeting("Pokemon List")
                     DemoScreen()
                 }
             }
@@ -43,6 +55,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
+        //alignment = Alignment.BottomCenter,
         modifier = modifier
     )
 }
@@ -51,16 +64,18 @@ fun DemoScreen() {
     var isOn by remember {
         mutableStateOf(false)
     }
-
-    Box(Modifier.fillMaxSize(), Alignment.TopCenter) {
+    var filterisOn by remember {
+        mutableStateOf(false)
+    }
+    Box(Modifier.size(64.dp), Alignment.TopEnd) {
         //Box(modifier = Modifier.size(40.dp), Alignment.TopCenter) {
         SearchButton(isOn) {
             isOn = !isOn
         }
     }
-        Box(Modifier.fillMaxSize(), Alignment.TopEnd) {
-        FilterButton(isOn) {
-            isOn = !isOn
+    Box(Modifier.size(128.dp), Alignment.TopCenter) {
+        FilterButton(filterisOn) {
+            filterisOn = !filterisOn
         }
     }
 }
@@ -75,15 +90,13 @@ fun SearchButton(isOn: Boolean, onClick: () -> Unit) {
         }
     }
 }
-
 @Composable
 fun FilterButton(isOn: Boolean, onClick: () -> Unit) {
-
     Button(onClick = onClick) {
         if (isOn) {
             Text(text = "Off")
         } else {
-            Text("On")
+            Text("❤️")
         }
     }
 }
