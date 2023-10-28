664886e09a8e2e170dbdbeab0ed57433b23f5bd4
Abdisamad Nuh <abdisamad.nuh@outlook.com>
2023-10-28 12:03:31 +0200

    Searchpage 2.0.0 Finished version

--- app/src/main/java/com/example/pokedex/SearchPage.kt
+++ app/src/main/java/com/example/pokedex/SearchPage.kt
@@ -35,6 +35,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
@@ -139,7 +140,8 @@ fun Backarrow() {
                        alignment = Alignment.Center,
                        modifier= Modifier
                            .background(Color.Transparent)
-                           .fillMaxSize(),
+                           .size(64.dp)
+                           .clip(shape = CircleShape)
 
                    )
 
