e26d7535b7eb424b358705c0ef58f68b84ba612e
zainabjh01 <91522375+zainabjh01@users.noreply.github.com>
2024-01-09 11:52:42 +0100

    Added checkmark

--- app/src/main/java/com/example/pokedex/presentation/userInterface/filterPage/FilterPage.kt
+++ app/src/main/java/com/example/pokedex/presentation/userInterface/filterPage/FilterPage.kt
@@ -1,48 +1,56 @@
 package com.example.pokedex.presentation.userInterface.filterPage
 
+//import androidx.compose.foundation.layout.ColumnScopeInstance.align
+//import com.example.pokedex.Presentation.UserInterface
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.material3.Text
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-//import androidx.compose.foundation.layout.ColumnScopeInstance.align
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-//import com.example.pokedex.Presentation.UserInterface
-import com.example.pokedex.presentation.userInterface.filterPage.ResetViewModel
 import com.example.pokedex.R
-
 import com.example.pokedex.presentation.navigation.Route
 import com.example.pokedex.presentation.theme.Font
-import com.example.pokedex.presentation.userInterface.filterPage.FilterViewModel
 
 class FilterPage : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -169,7 +177,7 @@ fun FilterPageContent(navController: NavHostController, viewModel: FilterViewMod
                                     containerColor = Color(0xFF58ABF6))
 
                         ) {
-                            Text(text = "Generations", fontFamily = Font.rudaFontFamily,)
+                            Text(text = "Generations", fontFamily = Font.rudaFontFamily)
                         }
 
                         // Checks clickable Gen button and opens up generation loop buttons and stuff.
@@ -177,7 +185,11 @@ fun FilterPageContent(navController: NavHostController, viewModel: FilterViewMod
                             Column(
                                 modifier = Modifier
                                     .fillMaxWidth()
-                                    .border(1.dp, Color.LightGray, shape = RoundedCornerShape(20.dp))
+                                    .border(
+                                        1.dp,
+                                        Color.LightGray,
+                                        shape = RoundedCornerShape(20.dp)
+                                    )
                                     .padding(16.dp)
                             ) {
                                 for (generation in 1..9) {
@@ -436,7 +448,8 @@ fun GenerationButton(
         modifier = Modifier.padding(16.dp)
     ) {
         //Maybe my memory is like a goldfish, but this memory works!
-        val buttonText = "Generation $generation" + if (isNameInGeneration) " ✅" else ""
+        //val checkedMark = "\u2713"
+        val buttonText = "Generation $generation" + if (isNameInGeneration) " \u2713" else ""
         Text(
             text = buttonText,
             fontFamily = Font.rudaFontFamily
